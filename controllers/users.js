04a19eaa85c2d3de8832f79a831886a176abf1e6
Robin-LCA <robin@austincodingacademy.com>
2023-08-22 10:51:56 -0500

    initial commit

---
+++ controllers/users.js
@@ -0,0 +1,306 @@
+const mysql = require('mysql')
+const db = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
+const getAllUsers = (req, res) => {
+  // SELECT ALL USERS
+ let sql = "select first_name, last_name, address, city, county, state, zip, phone1, phone2, email ";
+ sql += "from users u join usersAddress ua on u.id = ua.user_id join usersContact uc on u.id = uc.user_id";
+
+ db.query(sql, (err, rows) => {
+    if(err) {
+      console.log("getAllUsers query failed", err);
+      res.sendStatus(500); // it was server's fault
+    } else {
+      return res.json(rows);
+    }
+ })
+
+  // OLDER SYNTAX
+  // pool.query("SELECT * FROM users", (err, rows) => {
+  //   if (err) return handleSQLError(res, err)
+  //   return res.json(rows);
+  // })
+}
+
+const getUserById = (req, res) => {
+  // SELECT USERS WHERE ID = <REQ PARAMS ID>
+  // users/:id
+  // put the requested path id into a variable
+
+  let id = req.params.id // this will get whatever is sent from the front-end form in the id text field
+
+  let params = [id]  //you can have more than on ? in your query, and they have to be in order of use in the query
+
+  // check for a valid id
+  if(!id){
+    res.sendStatus(400); // their fault. didn't send a valid id
+    return;              // and stop. no reason to execute any more code
+  }
+
+  // THIS IS A BAD WAY TO DO A QUERY!!! - with the id (or any other request param) concatinated to the string
+  // Why? 
+  // and what if I add and drop table users
+  // This is called a SQL Injection attack
+  // let sql = "select first_name, last_name, address, city, county, state, zip, phone1, phone2, email ";
+  // sql += "from users u join usersAddress ua on u.id = ua.user_id join usersContact uc on u.id = uc.user_id ";
+  // sql += "where u.id = " + id;
+
+  // instead we use parameterized sql statements
+  let sql = "select first_name, last_name, address, city, county, state, zip, phone1, phone2, email ";
+  sql += "from users u join usersAddress ua on u.id = ua.user_id join usersContact uc on u.id = uc.user_id ";
+  sql += "where u.id = ?";
+
+  // the ? is a dynamic value that is restricted to a query parameter.
+  // The query parameter isn't combined with the main query until AFTER the query has been parsed,
+  // so there's no way the parameter can introduce unintended syntax
+
+  db.query(sql, params, (err,rows) => {
+    if(err){
+      console.log("getUserById query failed ", err);
+      res.sendStatus(500); // it was our fault
+    } else {
+      // we got results, but we got more than one row
+      if(rows.length > 1){
+        console.log("Returned more than 1 row for id ", id);
+        res.sendStatus(500); // server's fault, data integrity error
+      } else if(rows.length == 0){
+        // res.send(null);  // don't send anything back
+        // OR
+        res.status(404).send('User not found');
+      } else {
+        // success! we got one row
+        res.json(rows[0])  // if return just res.json(rows), it returns an array with one object
+                        // if I want to return just the array, I need to reference the index
+      }
+    }
+  })
+
+
+  // // WHAT GOES IN THE BRACKETS
+  // sql = mysql.format(sql, [])
+
+  // pool.query(sql, (err, rows) => {
+  //   if (err) return handleSQLError(res, err)
+  //   return res.json(rows);
+  // })
+}
+
+const createUserCallback = (req, res) => {
+  // INSERT INTO USERS FIRST AND LAST NAME 
+  //    when the id is created that we need for the user_id columns in the other two tables
+  //    so the first insert MUST complete before we can execute the other 2 queries
+  // INSERT INTO  usersContact user_id, phone1, phone2, email
+  // INSERT INTO usersAddress user_id, address, city, county, state, zip
+
+  // asynchronous: run something until it finishes whenever, and goes on to run other stuff while that first thing may or may not be finished
+  // sometimes you need code to run in a certain order, like fetch and promises
+  // we call this blocking code sometimes
+
+  // here's the kicker: mysql doesn't have built-in methods to create blocking code out of the box
+
+  // one way we handle this is to create nested callbacks that execute each query one at a time
+  // this can get complicated. we call this scenario callback hell
+
+  // CALLBACK HELL VERSION
+
+  // FIRST QUERY
+  let first = req.body.first_name;
+  let last = req.body.last_name;
+
+  let params = [first, last];
+  // I could also do this, but sometimes it gets long
+  // let params = [req.body.first_name, req.body.last_name]
+
+  let sql ="insert into users (first_name, last_name) values (?, ?)";
+
+  db.query(sql, params, (err,rows) => {
+    if(err){
+      console.log("createUser query failed ", err);
+      res.sendStatus(500);
+    } else {
+      // if we got, the first query executed
+      // postman check
+      //res.json(rows)
+
+      // SECOND QUERY
+      // I need the id of the user we just inserted. For this example, might have to use MAX(id) to get the id
+      let getId = rows.insertId;
+
+      let address = req.body.address;
+      let city = req.body.city;
+      let county = req.body.county;
+      let state = req.body.state;
+      let zip = req.body.zip;
+
+      params = [getId, address, city, county, state, zip]
+      
+      sql = "insert into usersAddress (user_id, address, city, county, state, zip) ";
+      sql += "values (?, ?, ?, ?, ?, ?)";
+
+      db.query(sql, params, (err,rows) => {
+        if(err){
+          console.log("insert into usersAddress query failed ", err);
+          res.sendStatus(500);
+        } else {
+          // second query worked
+          // postman check
+          //res.json(rows);
+
+          //THIRD QUERY usersContact info
+          let phone1 = req.body.phone1;
+          let phone2 = req.body.phone2;
+          let email = req.body.email;
+
+          params = [getId, phone1, phone2, email]
+
+          sql = "insert into usersContact (user_id, phone1, phone2, email) values (?, ?, ?, ?)";
+
+          db.query(sql, params, (err, rows) => {
+            if(err){
+              console.log("insert into usersContact failed ", err)
+              res.sendStatus(500);
+            } else {
+              // it worked
+              res.json(rows); //this is final return
+            }
+
+          })
+        }
+      })
+    }
+  })
+};
+
+//Promise Version
+const createUser = async (req, res) => {
+// sync uses promises (async/await)
+  // FIRST QUERY
+  let first = req.body.first_name;
+  let last = req.body.last_name;
+  let params = [first, last];
+  
+  let sql ="insert into users (first_name, last_name) values (?, ?)";
+
+  let results;
+
+  try {
+    results = await db.querySync(sql, params);
+    // postman check
+    // res.json(results);
+  } catch(err) {
+    console.log("insert into users failed ", err);
+    res.sendStatus(500);
+    return; // if the query didn't work, stop
+  }
+
+  // SECOND QUERY
+  let getId = results.insertId; // from the previous query
+
+  let address = req.body.address;
+  let city = req.body.city;
+  let county = req.body.county;
+  let state = req.body.state;
+  let zip = req.body.zip;
+
+  params = [getId, address, city, county, state, zip]
+  
+  sql = "insert into usersAddress (user_id, address, city, county, state, zip) values (?, ?, ?, ?, ?, ?)";
+
+  try {
+    results = await db.querySync(sql, params);
+    // postman check
+    //res.json(results);
+  } catch(err) {
+    console.log("insert into usersAddress failed ", err);
+    res.sendStatus(500);
+    return; //stop if there's an error
+  }
+
+  // THIRD QUERY
+    let phone1 = req.body.phone1;
+    let phone2 = req.body.phone2;
+    let email = req.body.email;
+
+    params = [getId, phone1, phone2, email]
+
+    sql = "insert into usersContact (user_id, phone1, phone2, email) values (?, ?, ?, ?)";
+
+    try {
+      results = await db.querySync(sql, params);
+      res.send("User created successfully");
+      // postman check
+      // res.json(results);
+    } catch(err) {
+      console.log("insert into usersContact failed ", err);
+      res.sendStatus(500);
+      return; //stop if there's an error
+    }
+
+}; // end createUser
+
+
+
+
+const updateUserById = (req, res) => {
+  // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
+  // PUT /users/:id
+  /**  test case
+   * 503 Ashley Brooks
+   * 
+   *  {
+   *   "first_name": "Melinda",
+   *   "last_name" : "Miller"
+   *  }
+   */
+  let id = req.params.id;
+  let first = req.body.first_name;
+  let last = req.body.last_name;
+
+  let params = [first, last, id];  // order matters
+
+  let sql = "update users set first_name = ?, last_name = ? where id = ?";
+
+  if(!id) {
+    res.sendStatus(400);
+    return;  // no matching id, so stop
+  }
+
+
+  db.query(sql, params, (err, rows) => {
+    if(err) {
+      console.log("update failed ", err)
+      res.sendStatus(500);
+    } else {
+      res.json(rows)
+    }
+
+  })
+
+}
+
+const deleteUserByFirstName = (req, res) => {
+  // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
+  let params = [req.params.first_name];
+  let sql = "delete from users where first_name = ?"
+
+  db.query(sql, params, (err,rows) => {
+    if(err) {
+      console.log("delete failed ", err);
+      res.sendStatus(500);
+    } else {
+      res.json( { message: `Deleted ${rows.affectedRows} users`} )
+
+    }
+  })
+
+ 
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUserByFirstName
+}
